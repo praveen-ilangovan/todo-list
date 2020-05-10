fea4b7d1d043899c4bea52b86fcc0ddac5cccd39
Praveen Ilangovan <praveenkumar.ilangovan@gmail.com>
2020-05-11 00:23:51 +0100

    Added priority

--- resources/js/main.js
+++ resources/js/main.js
@@ -21,6 +21,17 @@
 */
 
 /**
+ * User class
+ * 
+ * A class to hold the user information
+ */
+ class User {
+ 	constructor(name) {
+ 		this.name = name;
+ 	}
+ }
+
+/**
  * Style class
  * 
  * A class to hold the css elements. This class
@@ -33,6 +44,113 @@
  		this.fontFamily = fontFamily;
  		this.fontSize = fontSize;
  		this.fontWeight = fontWeight;
+ 	}
+ }
+
+/**
+ * Priority class
+ * 
+ * This defines the priority of a task
+ */
+ class Priority {
+ 	#name = null;
+ 	#value = 0;
+ 	#style = null;
+
+ 	constructor(name, value, style) {
+ 		this.#name = name;
+ 		this.#value = value;
+ 		this.#style = style;
+ 	}
+
+	/*
+		Name
+	*/
+
+	/**
+	 * Get the name of the priority.
+	 * name is of type String
+	 */
+	getName() {
+		return this.#name;
+	};
+
+	/*
+		Value
+	*/
+
+	/**
+	 * Get the value of the priority.
+	 * value is of type Number
+	 */
+	getValue() {
+		return this.#value;
+	};
+ }
+
+/**
+ * HighPriority class
+ * 
+ */
+ class HighPriority extends Priority {
+ 	constructor() {
+ 		const name = "High"
+ 		const value = 300
+
+ 		const color = "black";
+ 		const bgColor = "white";
+ 		const fontFamily = "Helvetica";
+ 		const fontSize = "20px";
+ 		const fontWeight = 660;
+
+ 		const style = new Style(color, bgColor,
+ 			fontFamily, fontSize, fontWeight);
+
+ 		super(name, value, style);
+ 	}
+ }
+
+/**
+ * MedPriority class
+ * 
+ */
+ class MedPriority extends Priority {
+ 	constructor() {
+ 		const name = "Med"
+ 		const value = 200
+
+ 		const color = "black";
+ 		const bgColor = "white";
+ 		const fontFamily = "Helvetica";
+ 		const fontSize = "15px";
+ 		const fontWeight = 300;
+
+ 		const style = new Style(color, bgColor,
+ 			fontFamily, fontSize, fontWeight);
+
+ 		super(name, value, style);
+ 	}
+ }
+
+ /**
+ * LowPriority class
+ * 
+ */
+ class LowPriority extends Priority {
+ 	constructor() {
+ 		const name = "Low"
+ 		const value = 100
+
+ 		const color = "black";
+ 		const bgColor = "white";
+ 		const fontFamily = "Helvetica";
+ 		const fontSize = "10px";
+ 		const fontWeight = 100;
+
+ 		const style = new Style(color, bgColor,
+ 			fontFamily, fontSize, fontWeight);
+
+ 		super(name, value, style);
  	}
  }
 
