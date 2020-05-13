8cc225c2a7f7f0635d90679e3bed7343f31250e2
Praveen Ilangovan <praveenkumar.ilangovan@gmail.com>
2020-05-13 01:22:12 +0100

    Tasks get auto populated in the webpage.

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,7 +5,7 @@
 * task is and its priority and its status.
 */
 class Task {
-	constructor(uid, text, priority="low",status="todo") {
+	constructor(uid, text, priority="Low",status="todo") {
 		this.uid = uid;
 		this.text = text;
 		this.priority = priority;
@@ -28,14 +28,16 @@ class TaskManager {
 	 *
 	 * @param {String} text
 	 */
-	static AddTask(text, priority="low") {
+	static AddTask(text, priority="Low") {
 		let uid = localStorage.getItem("COUNTER") || "0";
 
-		let task = new Task(uid, text);
+		let task = new Task(uid, text, priority);
 		localStorage.setItem(uid, JSON.stringify(task));
 
 		uid++;
 		localStorage.setItem("COUNTER", uid.toString());
+
+		return task;
 	}
 
 	/**
@@ -47,7 +49,7 @@ class TaskManager {
 		let tasks = [];
 		for (let i = 0; i < localStorage.length; i++) {
 			const key = localStorage.key(i);
-			if (key == "COUNTER") {
+			if (isNaN(key)) {
 				continue;
 			}
 			tasks.push(TaskManager.GetTask(key));
@@ -77,7 +79,7 @@ class TaskManager {
 
 
 // 
-function addItem(e) {
+function addTask(e) {
 
 	if (!addTaskTextInput.value) {
 		const errorMsg = document.getElementById("addTaskAlert");
@@ -89,9 +91,46 @@ function addItem(e) {
 
 		return;
 	}
-	console.log(addTaskPriorityOption.value);
+	
+	const task = TaskManager.AddTask(addTaskTextInput.value, addTaskPriorityOption.value);
+	createTaskElement(task);
+}
 
-	TaskManager.AddTask(addTaskTextInput.value, addTaskPriorityOption.value);
+function createTaskElement(task) {
+	let bg = "alert-danger";
+	let parent = highPriorityContents;
+
+	if (task.priority.toLowerCase() === "med") {
+		bg = "alert-warning";
+		parent = medPriorityContents;
+	} else if (task.priority.toLowerCase() === "low") {
+		bg = "alert-success";
+		parent = lowPriorityContents;	
+	}
+
+	// Create a new div element
+	let div = document.createElement("div");
+	div.className = "card card-body task-box mb-3";
+	div.classList.add(bg);
+
+	// Create a new p element
+	let p = document.createElement("p");
+	p.className = "card-text";
+
+	// Add a text node
+	let tnode = document.createTextNode(task.text);
+
+	p.appendChild(tnode);
+	div.appendChild(p);
+	parent.appendChild(div);
+}
+
+function populateTasks() {
+	const tasks = TaskManager.GetTasks();
+
+	for (const task of tasks) {
+		createTaskElement(task);
+	}
 }
 
 
@@ -100,5 +139,18 @@ const addTaskTextInput = document.getElementById("addTaskTextInput");
 const addTaskPriorityOption = document.getElementById("addTaskPriorityOption");
 const addTaskButton = document.getElementById("addTaskButton");
 
+const highPriorityContents = document.getElementById("highPriorityContents");
+const medPriorityContents = document.getElementById("medPriorityContents");
+const lowPriorityContents = document.getElementById("lowPriorityContents");
+
 // Event handlers
-addTaskButton.addEventListener("click", addItem);
+addTaskButton.addEventListener("click", addTask);
+
+// Necessary to initialize all popovers to work
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+
+// Load all tasks
+populateTasks();
